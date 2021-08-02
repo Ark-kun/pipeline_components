11972407a5b9c527d63b27b7e32b546eb40c7c47
Alexey Volkov <alexey.volkov@ark-kun.com>
2021-08-01 18:38:47 -0700

    Components - Google Cloud - Optimizer - Updated the samples

--- components/google-cloud/Optimizer/_samples/multi_stage_optimization_pipeline.py
+++ components/google-cloud/Optimizer/_samples/multi_stage_optimization_pipeline.py
@@ -1,12 +1,18 @@
+# This pipeline demonstrates hyper-parameter optimization.
+# This version of the optimization pipeline uses imperative components that are closer to the API but not pure, so their usage is not recommended.
+# Please use the google-cloud/Optimizer/Suggest_parameter_sets_based_on_measurements component
+# and the atomic pipeline https://github.com/Ark-kun/pipeline_components/blob/master/components/google-cloud/Optimizer/_samples/multi_stage_optimization_atomic_pipeline.py
+# instead.
+
 kfp_endpoint = None
 
-
+import datetime
 import kfp
 from kfp import components
 
-optimizer_create_study_op = components.load_component_from_url('https://raw.githubusercontent.com/kubeflow/pipelines/40e117cca61fd923a57a1e84cbd08c22dce4bf00/components/google-cloud/Optimizer/Create_study/component.yaml')
-optimizer_suggest_trials_op = components.load_component_from_url('https://raw.githubusercontent.com/kubeflow/pipelines/40e117cca61fd923a57a1e84cbd08c22dce4bf00/components/google-cloud/Optimizer/Suggest_trials/component.yaml')
-optimizer_add_measurement_op = components.load_component_from_url('https://raw.githubusercontent.com/kubeflow/pipelines/40e117cca61fd923a57a1e84cbd08c22dce4bf00/components/google-cloud/Optimizer/Add_measurement_for_trial/component.yaml')
+optimizer_create_study_op = components.load_component_from_url('https://raw.githubusercontent.com/Ark-kun/pipeline_components/b3ec80d64938960d8caa4f1ba1ae1bbcc583e0b2//components/google-cloud/Optimizer/Create_study/component.yaml')
+optimizer_suggest_trials_op = components.load_component_from_url('https://raw.githubusercontent.com/Ark-kun/pipeline_components/b3ec80d64938960d8caa4f1ba1ae1bbcc583e0b2//components/google-cloud/Optimizer/Suggest_trials/component.yaml')
+optimizer_add_measurement_op = components.load_component_from_url('https://raw.githubusercontent.com/Ark-kun/pipeline_components/b3ec80d64938960d8caa4f1ba1ae1bbcc583e0b2//components/google-cloud/Optimizer/Add_measurement_for_trial/component.yaml')
 
 get_element_by_index_op = components.load_component_from_url('https://raw.githubusercontent.com/kubeflow/pipelines/55ef28a9d51edc4eeed2a5c6f44cc7457e8a41d8/components/json/Get_element_by_index/component.yaml')
 get_element_by_key_op = components.load_component_from_url('https://raw.githubusercontent.com/kubeflow/pipelines/55ef28a9d51edc4eeed2a5c6f44cc7457e8a41d8/components/json/Get_element_by_key/component.yaml')
@@ -14,7 +20,7 @@ query_json_op = components.load_component_from_url('https://raw.githubuserconten
 
 
 # Component that builds a model given the [hyper]parameters and evaluates that model.
-# In this case, the model is a polinomial model.
+# In this case, the model is a polynomial model.
 # The evaluation procedure compares the model with the real function that our model is trying to learn
 # and calculates the mean squared error based on a random sample of data points.
 # In real world cases this component will be substituted by a sequence of model trainer, predictor and evaluator components.
@@ -48,9 +54,10 @@ def optimizer_pipeline(
 ):
     optimization_stages = 3
     trials_per_stage = 5
+    study_id='Study_' + datetime.datetime.utcnow().strftime("%Y-%m-%d_%H-%M-%S")
 
     study_name = optimizer_create_study_op(
-        study_id='Study4',
+        study_id=study_id,
         parameter_specs=[
             {
                 'parameter': 'p1',
@@ -87,7 +94,7 @@ def optimizer_pipeline(
     for stage in range(optimization_stages):
         suggest_trials_task = optimizer_suggest_trials_op(
             study_name=study_name,
-            suggestion_count=suggestion_count,
+            suggestion_count=trials_per_stage,
         )
         suggest_trials_task.after(*trial_measurement_tasks)
 
@@ -98,7 +105,7 @@ def optimizer_pipeline(
         # We need to collect the created tasks in the `trial_measurement_tasks` list so that the next round of suggestions can depend on their completion.
         # Cannot use dsl.ParallelFor here due to a bug in Argo https://github.com/argoproj/argo/issues/2660
         # Without ParallelFor we have to use python loop
-        # and explicitly get individual suggections using the get_element_by_index_op component
+        # and explicitly get individual suggestions using the get_element_by_index_op component
         # then extract the trial name and parameter sets using get_element_by_key_op and query_json_op components.
         trial_measurement_tasks = []
         for trial_index in range(trials_per_stage):
