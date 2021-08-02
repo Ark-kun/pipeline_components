1197613c37312be56b1343fb09d56c1bc702e363
Alexey Volkov <alexey.volkov@ark-kun.com>
2021-08-01 21:35:20 -0700

    Coogle Cloud - AutoML - Upgraded the Create dataset for tables component

--- components/gcp/automl/create_dataset_for_tables/component.py
+++ components/gcp/automl/create_dataset_for_tables/component.py
@@ -1,67 +1,65 @@
-# Copyright 2019 The Kubeflow Authors
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 from typing import NamedTuple
 
 
-def automl_create_dataset_for_tables(
-    gcp_project_id: str,
-    gcp_region: str,
+def create_dataset_for_google_cloud_automl_tables(
     display_name: str,
     description: str = None,
     tables_dataset_metadata: dict = {},
-    retry=None, #=google.api_core.gapic_v1.method.DEFAULT,
+    gcp_project_id: str = None,
+    gcp_region: str = None,
+    retry_config: dict = None, # : google.api_core.retry.Retry = google.api_core.gapic_v1.method.DEFAULT,
     timeout: float = None, #=google.api_core.gapic_v1.method.DEFAULT,
-    metadata: dict = None,
-) -> NamedTuple('Outputs', [('dataset_path', str), ('create_time', str), ('dataset_id', str), ('dataset_url', 'URI')]):
-    '''automl_create_dataset_for_tables creates an empty Dataset for AutoML tables
-    '''
+) -> NamedTuple('Outputs', [
+    ('dataset_name', str),
+    ('dataset', dict), #data_type='GoogleCloudAutoMlDataset'
+    ('dataset_path', str), # Deprecated
+    ('create_time', str), # Deprecated
+    ('dataset_id', str), # Deprecated
+    ('dataset_url', 'URI')
+]):
+    '''Creates an empty Dataset for AutoML tables'''
+    import logging
     import google
-    from google.cloud import automl
+    from google.protobuf import json_format
+    from google.cloud import automl_v1beta1 as automl
     client = automl.AutoMlClient()
 
-    location_path = client.location_path(gcp_project_id, gcp_region)
-    dataset_dict = {
-        'display_name': display_name,
-        'description': description,
-        'tables_dataset_metadata': tables_dataset_metadata,
-    }
+    if not gcp_project_id:
+        _, gcp_project_id = google.auth.default()
+
+    if not gcp_region:
+        gcp_region = 'us-central1'
+    if gcp_region != 'us-central1':
+        logging.warn('AutoML only supports the us-central1 region')
+
     dataset = client.create_dataset(
-        location_path,
-        dataset_dict,
-        retry or google.api_core.gapic_v1.method.DEFAULT,
-        timeout or google.api_core.gapic_v1.method.DEFAULT,
-        metadata,
+        parent=f"projects/{gcp_project_id}/locations/{gcp_region}",
+        dataset=automl.Dataset(
+            display_name=display_name,
+            description=description,
+            tables_dataset_metadata=tables_dataset_metadata,
+        ),
+        retry=google.api_core.retry.Retry(**retry_config) if retry_config else google.api_core.gapic_v1.method.DEFAULT,
+        timeout=timeout or google.api_core.gapic_v1.method.DEFAULT,
+        # ! metadata was dict before, but now it's a sequence of tuples.
+        #metadata=(metadata or {}).items(),
     )
-    print(dataset)
+    dataset_json = json_format.MessageToJson(dataset._pb)
+    print(dataset_json)
     dataset_id = dataset.name.rsplit('/', 1)[-1]
-    dataset_url = 'https://console.cloud.google.com/automl-tables/locations/{region}/datasets/{dataset_id}/schemav2?project={project_id}'.format(
-        project_id=gcp_project_id,
-        region=gcp_region,
-        dataset_id=dataset_id,
-    )
-    return (dataset.name, dataset.create_time, dataset_id, dataset_url)
+    dataset_url = f'https://console.cloud.google.com/automl-tables/locations/{gcp_region}/datasets/{dataset_id}/schemav2?project={gcp_project_id}'
+    print(dataset_url)
+    return (dataset.name, dataset_json, dataset.name, str(dataset.create_time), dataset_id, dataset_url)
 
 
 if __name__ == '__main__':
     from kfp.components import create_component_from_func
 
-    automl_create_dataset_for_tables_op = create_component_from_func(
-        automl_create_dataset_for_tables,
+    create_dataset_for_google_cloud_automl_tables_op = create_component_from_func(
+        create_dataset_for_google_cloud_automl_tables,
         output_component_file='component.yaml',
-        base_image='python:3.7',
-        packages_to_install=['google-cloud-automl==0.4.0'],
+        base_image='python:3.9',
+        packages_to_install=['google-cloud-automl==2.4.2'],
         annotations={
             "author": "Alexey Volkov <alexey.volkov@ark-kun.com>",
             "canonical_location": "https://raw.githubusercontent.com/Ark-kun/pipeline_components/master/components/gcp/automl/create_dataset_for_tables/component.yaml",
