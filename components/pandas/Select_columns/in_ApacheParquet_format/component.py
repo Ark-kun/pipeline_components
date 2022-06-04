0f0650b8446277b10f7ab48d220e413eef04ec69
Alexey Volkov <alexey.volkov@ark-kun.com>
2022-06-04 01:35:04 -0700

    pandas - Select_columns - Added the components
    * in_CSV_format
    * in_ApacheParquet_format

---
+++ components/pandas/Select_columns/in_ApacheParquet_format/component.py
@@ -0,0 +1,26 @@
+from kfp.components import InputPath, OutputPath, create_component_from_func
+
+
+def select_columns_using_Pandas_on_ApacheParquet_data(
+    table_path: InputPath("ApacheParquet"),
+    transformed_table_path: OutputPath("ApacheParquet"),
+    column_names: list,
+):
+    import pandas
+
+    df = pandas.read_parquet(table_path)
+    df = df[column_names]
+    df.to_parquet(transformed_table_path, index=False)
+
+
+if __name__ == "__main__":
+    select_columns_using_Pandas_on_ApacheParquet_data_op = create_component_from_func(
+        select_columns_using_Pandas_on_ApacheParquet_data,
+        output_component_file="component.yaml",
+        base_image="python:3.9",
+        packages_to_install=["pandas==1.4.1",],
+        annotations={
+            "author": "Alexey Volkov <alexey.volkov@ark-kun.com>",
+            "canonical_location": "https://raw.githubusercontent.com/Ark-kun/pipeline_components/master/components/pandas/Select_columns/in_ApacheParquet_format/component.yaml",
+        },
+    )
---
+++ components/pandas/Select_columns/in_CSV_format/component.py
@@ -0,0 +1,26 @@
+from kfp.components import InputPath, OutputPath, create_component_from_func
+
+
+def select_columns_using_Pandas_on_CSV_data(
+    table_path: InputPath("CSV"),
+    transformed_table_path: OutputPath("CSV"),
+    column_names: list,
+):
+    import pandas
+
+    df = pandas.read_csv(table_path)
+    df = df[column_names]
+    df.to_csv(transformed_table_path, index=False)
+
+
+if __name__ == "__main__":
+    select_columns_using_Pandas_on_CSV_data_op = create_component_from_func(
+        select_columns_using_Pandas_on_CSV_data,
+        output_component_file="component.yaml",
+        base_image="python:3.9",
+        packages_to_install=["pandas==1.4.2",],
+        annotations={
+            "author": "Alexey Volkov <alexey.volkov@ark-kun.com>",
+            "canonical_location": "https://raw.githubusercontent.com/Ark-kun/pipeline_components/master/components/pandas/Select_columns/in_CSV_format/component.yaml",
+        },
+    )
