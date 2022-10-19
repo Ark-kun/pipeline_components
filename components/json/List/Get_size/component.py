bc977fc4852b53f031830cdc5f7f31ee47226038
Alexey Volkov <alexey.volkov@ark-kun.com>
2022-10-18 22:22:29 -0700

    JSON - Added Get_size components for List and Dict

---
+++ components/json/List/Get_size/component.py
@@ -0,0 +1,20 @@
+from kfp.components import create_component_from_func
+
+
+def get_size_of_list(
+    list: list,
+) -> int:
+    """Gets size of a JSON array."""
+    return len(list)
+
+
+if __name__ == "__main__":
+    get_size_of_list_op = create_component_from_func(
+        get_size_of_list,
+        base_image="python:3.10",
+        output_component_file="component.yaml",
+        annotations={
+            "author": "Alexey Volkov <alexey.volkov@ark-kun.com>",
+            "canonical_location": "https://raw.githubusercontent.com/Ark-kun/pipeline_components/master/components/json/List/Get_size/component.yaml",
+        },
+    )
