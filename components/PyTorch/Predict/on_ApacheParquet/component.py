7799eb6ae4c4bb817f7461e2e3ffc83cbe2d50a7
Alexey Volkov <alexey.volkov@ark-kun.com>
2025-12-01 03:44:31 -0800

    feat: PyTorch - Predict - Added the prediction components (CSV, ApacheParquet)

---
+++ components/PyTorch/Predict/on_ApacheParquet/component.py
@@ -0,0 +1,75 @@
+from cloud_pipelines.components import InputPath, OutputPath, create_component_from_func
+
+
+def predict_with_PyTorch_model_on_ApacheParquet_data(
+    data_path: InputPath("ApacheParquet"),
+    model_path: InputPath("PyTorchScriptModule"),
+    predictions_path: OutputPath("ApacheParquet"),
+    label_column_name: str = None,
+    prediction_column_name: str = "prediction",
+    batch_size: int = 100,
+):
+    """Makes predictions using a trained PyTorch model.
+
+    Args:
+        data_path: Feature data in CSV format.
+        model_path: Trained model in binary PyTorchScriptModule format.
+        predictions_path: Model predictions.
+        label_column_name: Optional. Name of the column containing the label data that is excluded during the prediction.
+    """
+    from pathlib import Path
+
+    import numpy
+    import pandas
+    import torch
+
+    model = torch.jit.load(model_path)
+    # Switch the model to evaluation mode
+    model.eval()
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"{torch.cuda.is_available()=}")
+    print(f"{device=}")
+    model.to(device)
+
+    df = pandas.read_parquet(
+        data_path,
+    )
+    print("Evaluation data information:")
+    df.info(verbose=True)
+
+    if label_column_name is not None:
+        features_df = df.drop(columns=[label_column_name])
+    else:
+        features_df = df
+
+    # TODO: Batch processing
+    features_numpy = features_df.to_numpy(dtype=numpy.float32)
+    features_tensor = torch.tensor(features_numpy).to(device)
+    print(f"{features_tensor.shape=}")
+    predictions_tensor = model(features_tensor)
+    predictions = predictions_tensor.cpu().detach().numpy()
+
+    # Insert predictions column first
+    df.insert(loc=0, column=prediction_column_name, value=predictions)
+
+    Path(predictions_path).parent.mkdir(parents=True, exist_ok=True)
+    df.to_parquet(predictions_path)
+
+
+if __name__ == "__main__":
+    predict_with_PyTorch_model_on_ApacheParquet_data_op = create_component_from_func(
+        predict_with_PyTorch_model_on_ApacheParquet_data,
+        output_component_file="component.yaml",
+        base_image="pytorch/pytorch:1.7.1-cuda11.0-cudnn8-runtime",
+        packages_to_install=[
+            "pandas==2.0.3",
+            "numpy==1.24.4",
+            "pyarrow==17.0.0",
+        ],
+        annotations={
+            "author": "Alexey Volkov <alexey.volkov@ark-kun.com>",
+            "canonical_location": "https://raw.githubusercontent.com/Ark-kun/pipeline_components/master/components/PyTorch/Predict/on_CSV/component.yaml",
+            "python_dependencies_time": "2025-11-30",
+        },
+    )
---
+++ components/PyTorch/Predict/on_CSV/README.md
@@ -0,0 +1,61 @@
+<!-- BEGIN_GENERATED_CONTENT -->
+# Predict with PyTorch model on CSV data
+
+Description: Makes predictions using a trained PyTorch model.
+
+Author: Alexey Volkov <alexey.volkov@ark-kun.com>
+
+Location: [GitHub](https://github.com/Ark-kun/pipeline_components/blob/master/components/PyTorch/Predict/on_CSV/component.yaml), [Raw](https://raw.githubusercontent.com/Ark-kun/pipeline_components/master/components/PyTorch/Predict/on_CSV/component.yaml)
+
+## Inputs
+
+|Name|Type|Default|Description|
+|-|-|-|-|
+|**data** **\***|[CSV]||Feature data in CSV format.|
+|**model** **\***|[PyTorchScriptModule]||Trained model in binary PyTorchScriptModule format.|
+|label_column_name|[String]||Optional. Name of the column containing the label data that is excluded during the prediction.|
+|prediction_column_name|[String]|prediction||
+|batch_size|[Integer]|100||
+
+## Outputs
+
+|Name|Type|Description|
+|-|-|-|
+|predictions|[CSV]|Model predictions.|
+
+## Implementation
+
+#### Container
+
+Container image: [pytorch/pytorch:1.7.1-cuda11.0-cudnn8-runtime](https://hub.docker.com/r/pytorch/pytorch)
+
+## Usage
+
+```python
+predict_with_PyTorch_model_on_CSV_data_op = components.load_component_from_url("https://raw.githubusercontent.com/Ark-kun/pipeline_components/master/components/PyTorch/Predict/on_CSV/component.yaml")
+...
+predict_with_PyTorch_model_on_CSV_data_task = predict_with_PyTorch_model_on_CSV_data_op(
+    data=...,
+    model=...,
+    # Optional:
+    # label_column_name=...,
+    # prediction_column_name="prediction",
+    # batch_size=100,
+)
+```
+
+## Other information
+
+###### Tags
+
+* input_type=[CSV]
+* input_type=[Integer]
+* input_type=[PyTorchScriptModule]
+* input_type=[String]
+* output_type=[CSV]
+
+[CSV]: https://github.com/Ark-kun/pipeline_components/tree/master/types/CSV
+[Integer]: https://github.com/Ark-kun/pipeline_components/tree/master/types/Integer
+[PyTorchScriptModule]: https://github.com/Ark-kun/pipeline_components/tree/master/types/PyTorchScriptModule
+[String]: https://github.com/Ark-kun/pipeline_components/tree/master/types/String
+<!-- END_GENERATED_CONTENT -->
